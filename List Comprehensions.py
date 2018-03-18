4b9ac08139a24e5ddb9031fb632e750799f69114
Steven A. Flowers <stevenanthonyflowers@gmail.com>
2018-03-18 14:11:56 -0400

    Update with flat comprehension

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,3 +1,7 @@
 m = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
-r = [[x[y] for x in m] for y in range(len(m)+1)]
-print(r)
+# Transpose array
+t = [[x[y] for x in m] for y in range(len(m)+1)]
+print(t)
+# Flatten array
+f = [x[y] for x in m for y in range(len(m)+1)]
+print(f)
